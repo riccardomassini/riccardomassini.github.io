aa296c076b7f95d23126cde55a97db962daf80d0
riccardomassini <118017977+riccardomassini@users.noreply.github.com>
2025-06-09 19:08:13 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navLinks = document.querySelectorAll('nav a');
     const sections = document.querySelectorAll('.section');
+    const matrixBackground = document.querySelector('.matrix-background');
 
     function showSection(id) {
         sections.forEach(section => {
@@ -11,7 +12,6 @@ document.addEventListener('DOMContentLoaded', function() {
         const activeSection = document.querySelector(id);
         if (activeSection) {
             activeSection.classList.add('active');
-
             setTimeout(() => {
                 activeSection.setAttribute('data-animated', 'true');
             }, 50);
@@ -38,4 +38,19 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 50);
     }
     document.querySelector('nav a[href="#about"]').classList.add('active-nav');
+
+    if (matrixBackground) {
+        const characters = "01010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101"; // Puoi variare i caratteri
+        const characterCount = 500;
+
+        for (let i = 0; i < characterCount; i++) {
+            const charSpan = document.createElement('span');
+            charSpan.textContent = characters[Math.floor(Math.random() * characters.length)];
+            charSpan.style.left = `${Math.random() * 100}%`;
+            charSpan.style.top = `${Math.random() * 100}%`;
+            charSpan.style.animationDelay = `${Math.random() * 5}s`;
+            charSpan.style.animationDuration = `${Math.random() * 8 + 3}s`;
+            matrixBackground.appendChild(charSpan);
+        }
+    }
 });
